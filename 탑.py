9acc93bf55fe86bb2ea7aa6c294f93d9fa7d6ef3
Factories-git <official.factories@gmail.com>
2024-12-28 13:17:31 +0900

    KOI

--- 탑.py
+++ 탑.py
@@ -1,19 +1,14 @@
 n = int(input())
 tower = list(map(int, input().split()))
 re = [0] * n
+new = [[j, i] for i, j in enumerate(tower)]
 stack = []
-for i in range(n-1, -1, -1):
-    if stack:
-        while stack[-1][0] <= tower[i]:
+for i in range(n):
+    while stack:
+        if stack[-1][0] <= new[i][0]:
             stack.pop()
-            if not stack:
-                break
-    if stack:
-        re[i] = stack[-1][1]
-    stack.append([tower[i], i])
-    print(stack, i)
-for i in re:
-    if i <= 0:
-        print(0, end=' ')
-        continue
-    print(i+1, end=' ')
+        else:
+            re[i] = stack[-1][1]+1
+            break
+    stack.append(new[i])
+print(*re)
