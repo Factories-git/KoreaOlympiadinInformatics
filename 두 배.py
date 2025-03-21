9bfc6a8f8c43b585a9d9028d5fe195dd48098549
Factories-git <official.factories@gmail.com>
2025-03-22 08:17:36 +0900

    KOI

--- 두 배.py
+++ 두 배.py
@@ -1,13 +1,15 @@
-#33점 받은 코드
+#12점 받은 코드 (시간초과는 아님)
+from math import log2 as log, ceil
+
 
 n = int(input())
 arr = list(map(int, input().split()))
 c = 0
 for i in range(1, n):
-    if arr[i-1] > arr[i]:
-        k = 0
-        while arr[i-1] > arr[i]:
-            arr[i] *= 2
-            c += 1
-            k += 1
-print(c)
+    if arr[i - 1] > arr[i]:
+        lg = ceil(log(arr[i - 1]) / arr[i])
+        arr[i] <<= lg
+        c += lg
+        print(lg)
+
+print(c, arr)
--- 카드 바꾸기.py
+++ 카드 바꾸기.py
@@ -2,9 +2,49 @@ from collections import Counter
 
 n = int(input())
 cards = list(map(int, input().split()))
-increase = 0
-decrease = 0
-for i in range(1,n):
-    if cards[i-1] + 1 != cards[i]:
-        cards[i] = cards[i-1]+1
-        increase += 1
+c = Counter(cards)
+increase = n
+decrease = n
+same = 0
+m_k = 0
+max_ = max(c.values())
+for k, v in c.items():
+    if v == max_:
+        m_k = k
+for i in cards:
+    if i != m_k:
+        same += 1
+
+for i in range(1, n):
+    new_card = cards.copy()
+    new_s = 0
+    diff = cards[i-1] - cards[i]
+    if diff == 0:
+        continue
+    for j in range(n-1, 0, -1):
+        if diff == new_card[j-1] - new_card[j]:
+            continue
+        new_card[j] = new_card[j-1] - diff
+        new_s += 1
+
+    if new_card[0] - new_card[1] != diff:
+        new_card[0] = new_card[0] - diff
+        new_s += 1
+    decrease = min(new_s, decrease)
+
+for i in range(1, n):
+    new_card = cards.copy()
+    new_s = 0
+    diff = cards[i-1] - cards[i]
+    if diff == 0:
+        continue
+    if new_card[0] - new_card[1] != diff:
+        new_card[0] = new_card[0] - diff
+        new_s += 1
+    for j in range(1, n):
+        if diff == new_card[j-1] - new_card[j]:
+            continue
+        new_card[j] = new_card[j-1] - diff
+        new_s += 1
+    increase = min(new_s, increase)
+print(min(increase, decrease, same))
