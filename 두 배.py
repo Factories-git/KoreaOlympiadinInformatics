fb2499722de2e05fa5262c21a458c6a367f2dca8
Factories-git <official.factories@gmail.com>
2025-03-28 21:03:50 +0900

    KOI

--- 두 배.py
+++ 두 배.py
@@ -1,16 +1,14 @@
-#런타임 에러(33점)
 from math import log2 as log, ceil
 
 n = int(input())
 arr = list(map(int, input().split()))
 c = 0
-remember = arr[0]
+remember = 0
 for i in range(1, n):
-    if remember > arr[i]:
-        lg = ceil(log(remember / arr[i]))
-        remember = arr[i] << lg
+    lg = ceil(log(arr[i-1] / arr[i])) + remember
+    if 0 < lg:
+        remember = lg
         c += lg
     else:
-        remember = arr[i]
-
+        remember = 0
 print(c)
