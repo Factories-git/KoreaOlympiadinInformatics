916d00cd828d8396611c7abb84974421430e8abb
Factories-git <official.factories@gmail.com>
2025-03-29 16:11:42 +0900

    KOI

---
+++ 호숫가의 개미굴.py
@@ -0,0 +1,24 @@
+#21점 나온 코드
+
+n = int(input())
+rooms = list(map(int, input().split()))
+c = 0
+used = [False] * n
+dp = [0] * n
+for i in range(n):
+    if used[i]:
+        dp[i] = dp[i-1]
+        continue
+    k = (i+1) % n
+    p = rooms[k] + (1 if rooms[k] == 0 else 0)
+    j = rooms[i]
+    if j != 0:
+        dp[i] = dp[i-1] + j
+    else:
+        if rooms[k] == 0:
+            used[k] = True
+            if i == 0 and used[-1] == 0:
+                used[-1] = True
+        dp[i] = dp[i-1] + 1
+    used[i] = True
+print(max(dp))
