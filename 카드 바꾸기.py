eacebb23f9ab28db01a049076fbab775f83733b8
Code-Factory <official.factories@gmail.com>
2026-01-02 19:31:22 +0900

    KOI

--- 카드 바꾸기.py
+++ 카드 바꾸기.py
@@ -1,50 +1,46 @@
 from collections import Counter
 
 n = int(input())
-cards = list(map(int, input().split()))
-c = Counter(cards)
-increase = n
+cards = [0] + list(map(int, input().split()))
+counter = {}
 decrease = n
+increase = n
 same = 0
-m_k = 0
-max_ = max(c.values())
-for k, v in c.items():
-    if v == max_:
-        m_k = k
-for i in cards:
-    if i != m_k:
-        same += 1
-
-for i in range(1, n):
+for i in range(1, len(cards)):
+    for j in range(1, len(cards)):
+        if cards[i] != cards[j]:
+            same += 1
+#증가하는 순서대로 카드 바꾸기
+for i in range(1, len(cards)):
     new_card = cards.copy()
     new_s = 0
-    diff = cards[i-1] - cards[i]
+    diff = cards[i] - cards[i-1]
     if diff == 0:
         continue
-    for j in range(n-1, 0, -1):
-        if diff == new_card[j-1] - new_card[j]:
+    for j in range(2, len(cards)):
+        if new_card[j] - new_card[j-1] == diff:
             continue
-        new_card[j] = new_card[j-1] - diff
+        new_card[j] = new_card[j-1] + diff
         new_s += 1
-
-    if new_card[0] - new_card[1] != diff:
-        new_card[0] = new_card[0] - diff
-        new_s += 1
-    decrease = min(new_s, decrease)
-
-for i in range(1, n):
-    new_card = cards.copy()
-    new_s = 0
-    diff = cards[i-1] - cards[i]
-    if diff == 0:
-        continue
-    if new_card[0] - new_card[1] != diff:
-        new_card[0] = new_card[0] - diff
-        new_s += 1
-    for j in range(1, n):
-        if diff == new_card[j-1] - new_card[j]:
-            continue
-        new_card[j] = new_card[j-1] - diff
+    if new_card[2] - new_card[1] != diff:
+        new_card[1] = new_card[2] - diff
         new_s += 1
     increase = min(new_s, increase)
+    print(new_card, diff, new_s, i)
+#감소하는 순서대로
+for i in range(1, len(cards)):
+    new_card = cards.copy()
+    new_s = 0
+    diff = cards[i] - cards[i-1]
+    if diff == 0:
+        continue
+    if new_card[len(cards) - 1] - new_card[len(cards) - 2] != diff:
+        new_card[len(cards) - 1] = new_card[len(cards) - 2] + diff
+        new_s += 1
+    for j in range(len(new_card)-2, 0, -1):
+        if new_card[j+1] - new_card[j] == diff:
+            continue
+        new_card[j] = new_card[j+1] - diff
+        new_s += 1
+    decrease = min(new_s, decrease)
 print(min(increase, decrease, same))
--- 호숫가의 개미굴.py
+++ 호숫가의 개미굴.py
@@ -1,63 +1,12 @@
-#13점 나온 코드 (약 20%)
-#작성중
-"""
-반례:
-8
-0 0 1 1 1 0 0 0
-"""
-# def compare():
-#     c = rooms[k]
-#     if rooms[k] == 0:
-#         c = 1
-#     y = rooms[i]
-#     if y == 0:
-#         y = 1
-#     if dp[i - 1] + y >= dp[i - 1] + c:
-#         dp[i] = dp[i - 1] + y
-#         used[k] = True
-#     else:
-#         dp[i] = dp[i - 1]
-#
-#
-#
-# n = int(input())
-# rooms = list(map(int, input().split()))
-# used = [False] * n
-# dp = [0] * n
-# for i in range(n):
-#     if used[i]:
-#         dp[i] = dp[i-1]
-#         continue
-#     k = (i+1) % n
-#     j = rooms[i]
-#     if j != 0:
-#         dp[i] = dp[i-1] + 1
-#     else:
-#         compare()
-#     used[i] = True
-# print(max(dp))
-
-#밑은 21점 코드, 38%에서 틀림
-#위의 반례를 통과하지 못 함
 n = int(input())
-rooms = list(map(int, input().split()))
-c = 0
-used = [False] * n
-dp = [0] * n
+arr = list(map(int, input().split()))
+empty = [True] * n
+ants = 0
 for i in range(n):
-    if used[i]:
-        dp[i] = dp[i-1]
+    if arr[i]:
+        ants += arr[i]
         continue
-    k = (i+1) % n
-    p = rooms[k] + (1 if rooms[k] == 0 else 0)
-    j = rooms[i]
-    if j != 0:
-        dp[i] = dp[i-1] + j
-    else:
-        if rooms[k] == 0:
-            used[k] = True
-            if i == 0 and used[-1] == 0:
-                used[-1] = True
-        dp[i] = dp[i-1] + 1
-    used[i] = True
-print(max(dp))
+    if empty[i-1] and empty[(i+1) % n]:
+        ants += 1
+        empty[i] = False
+print(ants)
